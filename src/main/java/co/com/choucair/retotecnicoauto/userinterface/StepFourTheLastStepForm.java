a94166ad9b5c04d49489c912db201b3716c9a767
Diannys Granadillo <ddgranadillo31@gmail.com>
2021-05-11 18:50:38 -0500

    Se termina de implementar el cuarto paso para crear la cuenta

--- src/main/java/co/com/choucair/retotecnicoauto/userinterface/StepFourTheLastStepForm.java
+++ src/main/java/co/com/choucair/retotecnicoauto/userinterface/StepFourTheLastStepForm.java
@@ -5,9 +5,9 @@ import org.openqa.selenium.By;
 
 public class StepFourTheLastStepForm {
     public static final Target CREATE_YOUR_PASSWORD = Target.the("Crea tu contraseña")
-            .located(By.id("firstName"));
+            .located(By.id("password"));
     public static final Target CONFIRM_PASSWORD = Target.the("Confirma tu contraseña")
-            .located(By.id("firstName"));
+            .located(By.id("confirmPassword"));
     public static final Target TERM_OF_USER_CHECK = Target.the("Confirmar terminos de usuario")
             .located(By.id("termOfUse"));
     public static final Target PRIVACY_SECURITY_POLICY_CHECK = Target.the("Confirmar terminos de usuario")
