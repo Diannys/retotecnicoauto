a5baca0ea73775023d38f7307893710909a44fdb
Diannys Granadillo <ddgranadillo31@gmail.com>
2021-05-11 15:24:25 -0500

    Se escoge la opcion Fonseca, La Guajira en el campo City del segundo formulario

--- src/main/java/co/com/choucair/retotecnicoauto/tasks/StepTwoAddYourAddress.java
+++ src/main/java/co/com/choucair/retotecnicoauto/tasks/StepTwoAddYourAddress.java
@@ -5,6 +5,7 @@ import co.com.choucair.retotecnicoauto.userinterface.StepTwoAddYourAddressForm;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.Tasks;
+import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.Enter;
 
 public class StepTwoAddYourAddress implements Task {
@@ -15,6 +16,7 @@ public class StepTwoAddYourAddress implements Task {
     @Override
     public <T extends Actor> void performAs(T actor) {
         actor.attemptsTo(Enter.theValue("Fonseca, La Guajira").into(StepTwoAddYourAddressForm.lOCATION_CITY));
+        actor.attemptsTo(Click.on(StepTwoAddYourAddressForm.FONSECA_CITY_OPTION));
         actor.attemptsTo(Enter.theValue("0000").into(StepTwoAddYourAddressForm.POSTAL_CODE));
     }
 }
--- src/main/java/co/com/choucair/retotecnicoauto/userinterface/StepTwoAddYourAddressForm.java
+++ src/main/java/co/com/choucair/retotecnicoauto/userinterface/StepTwoAddYourAddressForm.java
@@ -6,7 +6,8 @@ import org.openqa.selenium.By;
 public class StepTwoAddYourAddressForm {
     public static final Target lOCATION_CITY = Target.the("Tu ubicacion")
             .located(By.id("city"));
-
+    public static final Target FONSECA_CITY_OPTION = Target.the("Tu ubicacion")
+            .located(By.xpath("//span[contains(text(), 'Fonseca, La Guajira')]"));
     public static final Target POSTAL_CODE = Target.the("Codigo Postal")
             .located(By.id("zip"));
 }
