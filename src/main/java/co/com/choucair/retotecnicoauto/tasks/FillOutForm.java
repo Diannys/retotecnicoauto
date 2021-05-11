8bf386d9d676f90e93a94afe98651c5f714d8c65
Diannys Granadillo <ddgranadillo31@gmail.com>
2021-05-11 02:16:42 -0500

    Se implementa el tercer paso: diligenciar formulario para crear cuenta.

--- src/main/java/co/com/choucair/retotecnicoauto/tasks/FillOutForm.java
+++ src/main/java/co/com/choucair/retotecnicoauto/tasks/FillOutForm.java
@@ -1,8 +1,21 @@
 package co.com.choucair.retotecnicoauto.tasks;
 
 import co.com.choucair.retotecnicoauto.model.RegistrationModel;
+import co.com.choucair.retotecnicoauto.userinterface.RegistrationForm;
+import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.Tasks;
+import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.actions.Enter;
+import net.serenitybdd.screenplay.actions.EnterValue;
 
-public class FillOutForm {
-    //public static RegistrationModel
+public class FillOutForm implements Task {
+    public static FillOutForm theForm() {
+        return Tasks.instrumented(FillOutForm.class);
+    }
+
+    @Override
+    public <T extends Actor> void performAs(T actor) {
+        actor.attemptsTo(Enter.theValue("llenar campo de nombre").into(RegistrationForm.FIRST_NAME));
+    }
 }
