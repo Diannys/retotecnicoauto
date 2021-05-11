1b2f641bfa72c1446161e5f288bc0ec90c797825
Diannys Granadillo <ddgranadillo31@gmail.com>
2021-05-10 23:42:38 -0500

    Se corrige ruta del webdriver en el archivo serenty.properties se corrige  la url Default en el archivo uTest

--- src/main/java/co/com/choucair/retotecnicoauto/userinterface/uTest.java
+++ src/main/java/co/com/choucair/retotecnicoauto/userinterface/uTest.java
@@ -3,7 +3,7 @@ package co.com.choucair.retotecnicoauto.userinterface;
 import net.serenitybdd.core.pages.PageObject;
 import net.thucydides.core.annotations.DefaultUrl;
 
-@DefaultUrl("https://www.utest.com/signup/personal")
+@DefaultUrl("https://www.utest.com/")
 public class uTest extends PageObject {
 
 }
